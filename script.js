9fa742a94c6e85ccf6f49769838d59b4158577a3
wa1ead <waliddraa2@gmail.com>
2024-11-03 20:08:21 +0100

    form validation in javascript

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const form = document.querySelector("form");
 
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const nameVal = document.getElementById("name").value;
+  const emailVal = document.getElementById("email").value;
+  const ageVal = document.getElementById("age").value;
+
+  console.log(nameVal);
+
+  errorMessages.innerHtml = "";
+
+  if (!nameVal) {
+    alert("Please enter your full name!");
+    return;
+  }
+  if (ageVal < 18) {
+    alert("Age must be greater than 18");
+    return;
+  }
+
+
+  form.submit();
+  form.reset();
+});
